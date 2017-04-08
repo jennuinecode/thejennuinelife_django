ec70f300f42b4688fbff7743bfbe404a59cd8093
jennuinecode <jennifer.cortes@hotmail.com>
2017-04-07 19:54:05 -0500

    removed add blog functionality for now

--- apps/blog/views.py
+++ apps/blog/views.py
@@ -5,31 +5,6 @@ def index(request):
 
     return render(request, 'blog/index.html')
 
-def add(request):
-
-    if request.method == "GET":
-
-        return render(request, 'blog/add.html')
-    else:
-
-        Blog.objects.create(title=request.POST['title'],  category=request.POST['category'], entry=request.POST['entry'])
-
-        context= {
-            'blogs': Blog.objects.all()
-        }
-
-        return render(request, 'blog/index.html', context)
-
-
-def delete(request, id):
-
-    context= {
-        'blogs': Blog.objects.filter(id=id)
-    }
-    course = Blog.objects.get(id=id)
-
-    return render(request, 'blog/index.html', context)
-
 
 def youcodegirl(request):
 
@@ -38,7 +13,7 @@ def youcodegirl(request):
 def myphotographystory(request):
 
     return render(request, 'blog/posts/thegrind/myphotographystory.html')
-    
+
 
 def fitnessstorypt1(request):
 
